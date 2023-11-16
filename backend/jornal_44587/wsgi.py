1c6ef263f208560d0159f691638f6a5a4aa2b5cb
Crowdbotics <team@crowdbotics.com>
2023-11-16 12:40:27 +0000

    Initial commit

---
+++ backend/jornal_44587/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for jornal_44587 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "jornal_44587.settings"
+)
+
+application = get_wsgi_application()
